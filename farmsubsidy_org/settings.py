76068e17537f0167abcf1842ff14640235883229
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-05-01 19:41:33 +0200

    Fix default database path

--- farmsubsidy_org/settings.py
+++ farmsubsidy_org/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'farmsubsidy_org.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite://' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600
     )
 }
